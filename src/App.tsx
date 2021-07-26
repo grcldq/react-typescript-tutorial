e94b66b9786e883f83c495c6dcf2499cd0c21b14
Geraldine Atayan <geraldineatayan@gmail.com>
2021-07-26 17:04:03 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+interface IState {
+  people: {
+    name: string,
+    url: string,
+    age: number,
+    note?: string //optional
+  }[]
+}
+
+const App = () => {
+  // const [number, setNumber] = useState<number | string>(5);
+  // const changeNumber = () => {
+  //   setNumber("10");
+  // };
+
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: 'LeBron James',
+  //     url: '',
+  //     age: 36,
+  //     note: 'Allergic to staying on the same team.',
+  //   },
+  //   {
+  //     name: 'Kobe Bryant',
+  //     url: '',
+  //     age: 36,
+  //   }
+  // ]);
+
+  const [people, setPeople] = useState<IState['people']>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>People Invited to my Party</h1>
     </div>
   );
-}
+};
 
 export default App;
